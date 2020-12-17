51b3f9f0059e7cee0cccd1fea2c034a8974dfa7c
taekyung <kiki9510@gmail.com>
2020-12-18 00:08:44 +0900

    django 16gang

--- accountapp/views.py
+++ accountapp/views.py
@@ -5,5 +5,9 @@ from django.shortcuts import render
 # Create your views here.
 
 def hello_world(request):
-    return render(request, 'accountapp/hello_world.html') # request를 받아서 내용을 base.html파일에서 가져온다.
-    # return HttpResponse('안녕하세요') # 해당 모듈이 없을 때, alt+enter를 누르면 해당 모듈이 어떤 라이브러리에 있는지 자동으로 찾아준다.
+
+    if request.method == "POST":
+        return render(request, 'accountapp/hello_world.html', context={'text': 'POST METHOD!!'}) # request를 받아서 내용을 base.html파일에서 가져온다.
+        # return HttpResponse('안녕하세요') # 해당 모듈이 없을 때, alt+enter를 누르면 해당 모듈이 어떤 라이브러리에 있는지 자동으로 찾아준다.
+    else:
+        return render(request, 'accountapp/hello_world.html', context={'text': 'GET METHOD!!'})
