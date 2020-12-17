9f2b37cfc7e89616dbf00c6f834f6887a432313d
taekyung <kiki9510@gmail.com>
2020-12-18 02:49:51 +0900

    django 18gang

--- accountapp/models.py
+++ accountapp/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 # Create your models here.
+# 데이터를 관리하는 모델(db와 연결)
 
 class HelloWorld(models.Model):
     text = models.CharField(max_length=255, null=False)
--- accountapp/views.py
+++ accountapp/views.py
@@ -1,8 +1,10 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 
 
 # Create your views here.
+from django.urls import reverse
+
 from accountapp.models import HelloWorld
 
 
@@ -10,13 +12,14 @@ def hello_world(request):
 
     if request.method == "POST":
 
-        temp = request.POST.get('hello_world_input')
+        temp = request.POST.get('hello_world_input') # hello_world_input에 작성된 내용을 temp에 담는다
 
         new_hello_world = HelloWorld()
         new_hello_world.text = temp
         new_hello_world.save() # db에 저장한다
 
-        return render(request, 'accountapp/hello_world.html', context={'hello_world_output': new_hello_world}) # request를 받아서 내용을 base.html파일에서 가져온다.
-        # return HttpResponse('안녕하세요') # 해당 모듈이 없을 때, alt+enter를 누르면 해당 모듈이 어떤 라이브러리에 있는지 자동으로 찾아준다.
+        return HttpResponseRedirect(reverse('accountapp:hello_world')) # accountapp안에 있는 hello_world의 url로 다시 돌아간다
     else:
-        return render(request, 'accountapp/hello_world.html', context={'text': 'GET METHOD!!'})
+        hello_world_list = HelloWorld.objects.all()
+        return render(request, 'accountapp/hello_world.html', context={'hello_world_list': hello_world_list}) # request를 받아서 내용을 base.html파일에서 가져온다.
+        # return HttpResponse('안녕하세요') # 해당 모듈이 없을 때, alt+enter를 누르면 해당 모듈이 어떤 라이브러리에 있는지 자동으로 찾아준다.
