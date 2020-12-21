f08d220aabcbfb3b585c7a954973c899e0fc3731
taekyung <kiki9510@gmail.com>
2020-12-21 23:39:18 +0900

    django 33gang

--- profileapp/views.py
+++ profileapp/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.urls import reverse_lazy
+from django.urls import reverse_lazy, reverse
 from django.utils.decorators import method_decorator
 from django.views.generic import CreateView, UpdateView
 
@@ -14,7 +14,7 @@ class ProfileCreateView(CreateView):
     model = Profile
     context_object_name = 'target_profile'
     form_class = ProfileCreationForm
-    success_url = reverse_lazy('accountapp:hello_world')
+    success_url = reverse_lazy('accountapp:detail') # 프로필정보 생성 후, detail페이지로 가도록 설정했지만, 아래에서 get_success_url함수를 작성해줘야 작동한다.
     template_name = 'profileapp/create.html'
 
     def form_valid(self, form):
@@ -23,6 +23,9 @@ class ProfileCreateView(CreateView):
         temp_profile.save()
         return super().form_valid(form)
 
+    def get_success_url(self):
+        return reverse('accountapp:detail', kwargs={'pk': self.object.user.pk}) # detail페이지로 가기 위해 object.user내에 pk를 찾아서 붙여준다.
+
 
 @method_decorator(profile_ownership_required, 'get')
 @method_decorator(profile_ownership_required, 'post')
@@ -30,5 +33,7 @@ class ProfileUpdateView(UpdateView):
     model = Profile
     context_object_name = 'target_profile'
     form_class = ProfileCreationForm
-    success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'profileapp/update.html'
+
+    def get_success_url(self):
+        return reverse('accountapp:detail', kwargs={'pk': self.object.user.pk})
