c70c5501b6fe133b78b41d74ec3f9be49156eedd
taekyung <kiki9510@gmail.com>
2020-12-23 00:36:07 +0900

    django 36gang

--- articleapp/urls.py
+++ articleapp/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
 from django.views.generic import TemplateView
 
-from articleapp.views import ArticleCreateView, ArticleDetailView, ArticleUpdateView, ArticleDeleteView
+from articleapp.views import ArticleCreateView, ArticleDetailView, ArticleUpdateView, ArticleDeleteView, ArticleListView
 
 app_name = 'articleapp'
 
 urlpatterns = [
-    path('list/', TemplateView.as_view(template_name='articleapp/list.html'), name='list'), # view에 작성한 내용 없으니까 연결할 템플릿 경로를 적어준다
+    path('list/', ArticleListView.as_view(), name='list'), # view에 작성한 내용 없으니까 연결할 템플릿 경로를 적어준다
     path('create/', ArticleCreateView.as_view(), name='create'),
     path('detail/<int:pk>', ArticleDetailView.as_view(), name='detail'),
     path('update/<int:pk>', ArticleUpdateView.as_view(), name='update'),
--- articleapp/views.py
+++ articleapp/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 # Create your views here.
 from django.urls import reverse, reverse_lazy
 from django.utils.decorators import method_decorator
-from django.views.generic import CreateView, DetailView, UpdateView, DeleteView
+from django.views.generic import CreateView, DetailView, UpdateView, DeleteView, ListView
 
 from articleapp.decorators import article_ownership_required
 from articleapp.forms import ArticleCreationForm
@@ -52,3 +52,10 @@ class ArticleDeleteView(DeleteView):
     context_object_name = 'target_article'
     success_url = reverse_lazy('articleapp:list')
     template_name = 'articleapp/delete.html'
+
+
+class ArticleListView(ListView):
+    model = Article
+    context_object_name = 'article_list' # html내에서 불러올 때 사용될 이름
+    template_name = 'articleapp/list.html' # 템플릿 경로(이름)
+    paginate_by = 25 # 한 페이지 내 게시물 수 제한
