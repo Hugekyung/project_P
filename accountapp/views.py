26db839b2cb1a56cf75bb1d9f838a58bf12e0d65
taekyung <kiki9510@gmail.com>
2021-01-04 19:12:14 +0900

    accountapp update

--- accountapp/views.py
+++ accountapp/views.py
@@ -21,7 +21,7 @@ has_ownership = [account_ownership_required, login_required]
 class AccountCreateView(CreateView):
     model = User # 계정 만들기
     form_class = UserCreationForm # 계정 회원가입 폼
-    success_url = reverse_lazy('home') # 성공 시 연결할 url
+    success_url = reverse_lazy('accountapp:login') # 성공 시 연결할 url
     # reverse를 클래스 내에서 그대로 사용할 수 없다(에러남)
     template_name = 'accountapp/create.html' # 어느 화면을 볼지
 
