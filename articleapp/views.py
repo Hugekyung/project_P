21c1626fc8815846f3555348b1d86ce485465e61
taekyung <kiki9510@gmail.com>
2021-02-07 18:01:14 +0900

    up

--- articleapp/views.py
+++ articleapp/views.py
@@ -1,7 +1,5 @@
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
 
-# Create your views here.
 from django.urls import reverse, reverse_lazy
 from django.utils.decorators import method_decorator
 from django.views.generic import CreateView, DetailView, UpdateView, DeleteView, ListView
